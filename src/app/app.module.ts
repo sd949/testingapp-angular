0ba32565a27927bdc2c09b1988111a25d2e37620
Swagata Das <swagatadas949@gmail.com>
2020-07-21 15:25:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {ServerComponent} from './server/server.component'
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+
   ],
   imports: [
     BrowserModule
